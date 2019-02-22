bbdebdbc7e679a2697e8bf573a102d6005e97c0b
Morten <morto1995@hotmail.com>
2019-02-22 11:53:29 +0100

    Logo

--- BodyTracker/Model/User.cs
+++ BodyTracker/Model/User.cs
@@ -9,11 +9,25 @@ namespace BodyTracker.Model
     class User
     {
         private string _name;
+        private double _weight;
+        private DateTime _lastLogon;
 
         public string Name
         {
             get { return _name; }
             set { _name = value; }
+        }
+
+        public DateTime LastLogon
+        {
+            get { return _lastLogon; }
+            set { _lastLogon = value; }
+        }
+
+        public double Weight
+        {
+            get { return _weight; }
+            set { _weight = value; }
         }
 
         public User(string name)
